e509358faffad9a6986f0611b0febc14ed35a74a
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2020-05-04 23:21:05 +0200

    app/libraries/Core.php get and instantiate controller from url

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,21 +1,38 @@
 <?php
-    /**
+    /* 
      * App Core Class
      * Creates URL & load core controller
      * URL FORMAT - /controller/method/params
      */
-    Class Core {
+    class Core {
         protected $currentController = 'Pages';
         protected $currentMethod = 'index';
         protected $params = [];
 
         public function __construct(){
-            $this->getUrl();
+            // print_r($this->getUrl());
+        
+            $url = $this->getUrl();
+            if(isset($url)){
+                if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+                    $this->currentController = ucwords($url[0]);
+                    unset($url[0]);
+                }
+            }
+
+            // echo $this->currentController;
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            $this->currentController = new $this->currentController();
         }
 
         public function getUrl(){
-            echo $_GET['url'];
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
-
     }
 ?>
