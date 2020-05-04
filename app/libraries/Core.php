6d90c96777536bc3b48ace6d48bf854937877e3f
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2020-05-04 20:36:59 +0200

    libraries/Core.php Core class implemented

---
+++ app/libraries/Core.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+     * App Core Class
+     * Creates URL & load core controller
+     * URL FORMAT - /controller/method/params
+     */
+    Class Core {
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = [];
+
+        public function __construct(){
+            $this->getUrl();
+        }
+
+        public function getUrl(){
+            echo $_GET['url'];
+        }
+
+    }
+?>
