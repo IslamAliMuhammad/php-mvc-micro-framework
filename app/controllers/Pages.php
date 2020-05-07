b71ad3f614023e4900fea800291837ad925e2185
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2020-05-07 20:35:41 +0200

    app/libraries/Controller.php Implemented as a base class for controller in controllers folder

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,11 @@
 <?php
-    class Pages {
+    class Pages extends Controller{
         public function __construct(){
             
         }
 
         public function index(){
-            echo 'hello I am  index function';
+
         }
 
         public function about($id){
---
+++ app/libraries/Controller.php
@@ -0,0 +1,24 @@
+<?php
+    class Controller {
+        /*
+         * Base Controller
+         * Loads the models and views 
+         */
+
+        public function model($model){
+
+            require_once '../models' . $model . '.php';
+            
+            return new $model();
+        }
+
+        public function view($view, $data = []){
+
+            if(file_exists('../views' . $view . '.php')){
+                require_once '../views' . $view . '.php';
+            }else {
+                die('View does not exist');
+            }
+        }
+    }
+?>
